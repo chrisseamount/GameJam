dabad2915bd96d19df87bb93a514a8c866616250
Chris <chris_seamount@hotmail.com>
2019-10-05 23:23:55 -0800

    added map gen

---
+++ GameJam/Assets/Scripts/FillRooms.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FillRooms : MonoBehaviour
+{
+    public LayerMask room;
+    public LevelGeneration levelGen;
+
+    private void Update()
+    {
+        Collider2D roomDetection = Physics2D.OverlapCircle(transform.position, 1, room);
+        if(roomDetection == null && levelGen.stopGeneration == true)
+        {
+            int rand = Random.Range(0, levelGen.rooms.Length);
+            Instantiate(levelGen.rooms[rand], transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
